e770376d263af7b23984c687f2d036c50f13603d
Michael Tobie Nnaji <tobiennaji@gmail.com>
2022-03-17 15:38:21 -0400

    done

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+const num1 = 62;
+
+const num2 = 1;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 29;
+
+const num4 = 5;
+
+const mod = num3 % num4;
+
+const max = Math.max(20,15,18,12);
+
